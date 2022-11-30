81d937fd7abee9ca74b169deb1020a3adf205733
Gaelle Fournier <gaelle.fournier.work@gmail.com>
2022-11-30 16:48:59 +0100

    WIP

--- 02-serverless-api/test/MinioCustomizer.java
+++ 02-serverless-api/test/MinioCustomizer.java
@@ -4,31 +4,34 @@ package test;
 import org.apache.camel.BindToRegistry;
 import org.apache.camel.PropertyInject;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+//import software.amazon.awssdk.services.s3.S3Configuration;
+import software.amazon.awssdk.services.s3.S3Client;
+
+import java.net.URI;
 
 public class MinioCustomizer {
 
     @BindToRegistry
-    public static AmazonS3 minioClient(
+    public static S3Client minioClient(
             @PropertyInject("minio.endpoint") String endpointAddress,
             @PropertyInject("minio.access-key") String accessKey,
             @PropertyInject("minio.secret-key") String secretKey) {
 
-        AwsClientBuilder.EndpointConfiguration endpoint = new AwsClientBuilder.EndpointConfiguration(endpointAddress, "US_EAST_1");
-        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-        AWSCredentialsProvider credentialsProvider = new AWSStaticCredentialsProvider(credentials);
+        AwsBasicCredentials awsCreds = AwsBasicCredentials.create(
+            accessKey,
+            secretKey);
 
-        return AmazonS3ClientBuilder
-            .standard()
-            .withEndpointConfiguration(endpoint)
-            .withCredentials(credentialsProvider)
-            .withPathStyleAccessEnabled(true)
+            // should work : https://docs.aws.amazon.com/sdk-for-java/latest/developer-guide/examples-s3.html
+            // or not since it is 2.18, so nouw let's try the 2.17 way
+        Region region = Region.US_EAST_1;
+        return S3Client.builder()
+            .region(region)
+            .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
+            .endpointOverride(URI.create(endpointAddress))
+            //.serviceConfiguration(S3Configuration.builder().pathStyleAccessEnabled(true).build())
             .build();
 
     }
